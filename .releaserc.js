6935f1edad09c8202d30ac36ad7c82dc13e86fd2
Alessandro Zanatta <alessandro.zanatta.lav@gmail.com>
2025-06-13 19:40:27 +0200

    ci: allow semantic-releases to create pre-releases from the dev branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["master"],
+  branches: ["master", { name: "dev", prerelease: true }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
