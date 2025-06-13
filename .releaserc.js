80978e5335c0847e170a4af04a2922e0d87f5c8e
Alessandro Zanatta <alessandro.zanatta.lav@gmail.com>
2025-06-13 22:01:53 +0200

    fix: allow semantic-releases to update README.md on Git

--- .releaserc.js
+++ .releaserc.js
@@ -19,7 +19,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "dist/**"],
+        assets: ["CHANGELOG.md", "dist/**", "README.md"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
