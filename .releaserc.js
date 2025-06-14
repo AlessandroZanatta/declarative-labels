4eb0ef930fbe7a4b8ce446642737e7e398defb05
Alessandro Zanatta <alessandro.zanatta.lav@gmail.com>
2025-06-14 09:59:16 +0200

    ci: update package.json version when releasing

--- .releaserc.js
+++ .releaserc.js
@@ -16,10 +16,11 @@ module.exports = {
           "yarn install --frozen-lockfile && yarn build && node ./scripts/update-readme-version.js ${nextRelease.version}",
       },
     ],
+    ["@semantic-release/npm"],
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "dist/**", "README.md"],
+        assets: ["CHANGELOG.md", "dist/**", "README.md", "package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
